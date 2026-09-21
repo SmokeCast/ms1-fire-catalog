89727dd23d62e88fe30be97b69bee17b78bd87a3
EmPuiquin24 <hepuiquin@proton.me>
2026-09-21 00:25:25 -0500

    chore: reorganiza y limpia todo el proyecto

--- smokecast/src/main/java/org/idea/smokecast/fireDetection/infrastructure/FireDetectionRepository.java
+++
@@ -1,7 +0,0 @@
-package org.idea.smokecast.fireDetection.infrastructure;
-
-import org.idea.smokecast.fireDetection.domain.FireDetection;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FireDetectionRepository extends JpaRepository<FireDetection, Long> {
-}
--- smokecast/src/main/java/org/idea/smokecast/fireEvent/infrastructure/FireEventRepository.java
+++
@@ -1,7 +0,0 @@
-package org.idea.smokecast.fireEvent.infrastructure;
-
-import org.idea.smokecast.fireEvent.domain.FireEvent;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FireEventRepository extends JpaRepository<FireEvent, Long> {
-}
